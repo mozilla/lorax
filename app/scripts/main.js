fb0522d5e5de062c2cadb4538f5c6571ee69aa36
Chris James <chris@work.co>
2014-10-16 17:56:27 -0400

    lobbying costs init

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -157,6 +157,62 @@ require(['jquery', 'd3'], function ($, d3) {
   });
 
 
+  // Increasing lobbying costs
+  /*var lobbyingCosts = function () {
+    var margin = {top: 48, right: 12, bottom: 88, left: 24};
+    var width = 600 - margin.left - margin.right;
+    var height = 540 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+      .range([0, width]);
+
+    var y = d3.scale.linear()
+      .range([height, 0]);
+
+    //var color = d3.scale.category10();
+
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
+
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
+
+    var line = d3.svg.line()
+      .interpolate('basis')
+      .x(function (d) { return x(d.year); })
+      .y(function (d) { return y(d.revolvers); });
+
+    var svg = d3.select('.lobbying-costs').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    d3.json('/scripts/data/lobbying-costs.json', function (error, res) {
+      var data = res.lobbyingCosts;
+
+      var varNames = d3.keys(data[0])
+        .filter(function (key) { console.log(key) });
+
+
+
+      var lobbying = chart.selectAll('g')
+        .data(data)
+        .enter()
+        .append('g');
+
+      lobbying.append('td')
+        .text(function (d) {
+          //return d.spoken.pct + '%';
+        });
+    });
+  };
+
+  // init
+  lobbyingCosts();*/
+
 
   // switch body background color depending on current issue
   // TODO: needs work, especially timing of transition relative to scroll
