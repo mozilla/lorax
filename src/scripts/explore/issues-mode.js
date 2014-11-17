024511b63111425f26bca6b091c232c37c49320f
Leandro Ferreira <durang@gmail.com>
2014-11-17 13:51:27 -0200

    touch scroll on issues

--- src/scripts/explore/issues-mode.js
+++ src/scripts/explore/issues-mode.js
@@ -26,6 +26,7 @@ define([
 
     IssuesMode.prototype.init = function () {
         this._scrollPosition = this._scrollFinalPosition = 0;
+        this._touchPosition =  this._touchDelta = this._touchFinalDelta = 0;
 
         this._issueMargin = 80;
 
@@ -80,6 +81,36 @@ define([
         this._scrollTo(event.deltaY / 5);
     };
 
+    IssuesMode.prototype._onTouching = function (event) {
+        var newPosition = event.getLocalPosition(this._canvas._stage).y;
+        newPosition -= this._canvas.canvasSize.y / 2;
+
+        this._touchDelta = this._touchFinalDelta = newPosition - this._touchPosition;
+        this._touchPosition = newPosition;
+
+        this._scrollTo(this._touchDelta);
+    };
+
+    IssuesMode.prototype._onTouchStart = function (event) {
+        if (this._touchInterval) {
+            clearInterval(this._touchInterval);
+        }
+
+        this._touchPosition = event.getLocalPosition(this._canvas._stage).y;
+        this._touchPosition -= this._canvas.canvasSize.y / 2;
+    };
+
+    IssuesMode.prototype._onTouchEnd = function (event) {
+        this._touchInterval = setInterval(function () {
+            this._touchDelta += (this._touchFinalDelta - this._touchDelta) / 5;
+            this._scrollTo(this._touchDelta);
+
+            if (Math.abs(this._touchFinalDelta - this._touchDelta) < 1) {
+                clearInterval(this._touchInterval);
+            };
+        }.bind(this), 1000 / 15);
+    };
+
     IssuesMode.prototype._scrollTo = function (delta) {
         this._scrollFinalPosition += delta;
 
@@ -94,7 +125,7 @@ define([
         var i, issue;
         for (i = 0; i < this._canvas.issues.length; i ++) {
           issue = this._canvas.issues[i];
-          issue.elm.y = issue.issueY + this._scrollPosition;
+          issue.elm.y = Math.round(issue.issueY + this._scrollPosition);
         }
     };
 
@@ -117,7 +148,10 @@ define([
         this._drawLinesBind = this._drawLines.bind(this);
         this._canvas.renderStartS.add(this._drawLinesBind);
 
-        $('body').on('mousewheel', this._onMouseWheel.bind(this));
+        $(document).on('mousewheel', this._onMouseWheel.bind(this));
+        this._canvas._stage.touchstart = this._onTouchStart.bind(this);
+        this._canvas._stage.touchend = this._onTouchEnd.bind(this);
+        this._canvas._stage.touchmove = this._onTouching.bind(this);
 
         setTimeout(this._onShow.bind(this), 500);
     };
