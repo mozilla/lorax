e432998e89918449c5c57e2dcdfec889eedcbc65
Leandro Ferreira <durang@gmail.com>
2014-12-05 17:50:20 -0200

    fixed svg on ff

--- src/app/lorax/directives/issue-all.js
+++ src/app/lorax/directives/issue-all.js
@@ -133,6 +133,12 @@ define(['jquery', 'webfontloader'], function ($, WebFont) {
 
         this._experienceService.setMenuPositions();
 
+        // FIXME: redirect svg fill so it works on ff
+        var fill = $('rect', '.banner-nav-bg');
+        var fillValue = fill.attr('fill');
+        fill.attr('fill', '');
+        fill.attr('fill', fillValue);
+
         if (detailIsHidden) {
             $('#detail').addClass('ng-hide');
         }
